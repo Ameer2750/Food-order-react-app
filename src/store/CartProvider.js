9a5f2bf322e54316842593116dc6cc46930fbd2a
Ameer <ameerbsc2630@gmail.com>
2023-08-20 10:54:00 +0530

    Code cleaup

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -16,7 +16,6 @@ const cartReducer = (state, action) => {
         )
 
         const existingCartItem = state.items[existingCartItemIndex];
-        // let updatedItem;
         let updatedItems;
 
         if (existingCartItem) {
